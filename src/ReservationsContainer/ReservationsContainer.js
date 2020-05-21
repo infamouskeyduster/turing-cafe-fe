874698e7befb0904c2cbc0a5fe912f70300de828
John Watterson <john@breakthrewbrew.com>
2020-05-21 10:03:13 -0600

    :tada: :crystal_ball: Add ReservationsContainer - this component renders a container that holds all reservation cards, it will render a card for each reservation passed to it as props

---
+++ src/ReservationsContainer/ReservationsContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReservationCard from '../ReservationCard/ReservationCard';
+
+const ReservationsContainer = ({reservations}) => {
+  let currentReservation = reservations.map(reservation => {
+    return (
+      <ReservationCard key={reservation.number + reservation.name} reservation={reservation}/>
+    )
+  })
+  // console.log('currentReservation', currentReservation);
+  return (
+    <section className="reservations-container">
+      {currentReservation}
+    </section>
+  )
+}
+
+export default ReservationsContainer;
