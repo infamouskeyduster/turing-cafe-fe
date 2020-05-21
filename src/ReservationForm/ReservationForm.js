c7629ab1a28db33ed7b605364156a519ad20347f
John Watterson <john@breakthrewbrew.com>
2020-05-21 11:56:54 -0600

    :crystal_ball: Add functionality that sets state on ReservationForm with user inputs, then upon hitting the 'make reservation' btn, the info is sent to the App component to set state

--- src/ReservationForm/ReservationForm.js
+++ src/ReservationForm/ReservationForm.js
@@ -2,29 +2,54 @@ import React, { Component } from 'react';
 import './ReservationForm.css';
 
 class ReservationForm extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      name: null,
-      date: null,
-      time: null,
-      numberOfGuests: null,
+      name: '',
+      date: '',
+      time: '',
+      number: '',
+      formReady: false,
     }
   }
 
-  updateStateWithUserInput = () => {
-    console.log('user is typing');
+  updateStateWithUserInput = (event) => {
+    event.preventDefault();
+    let targetedProperty = event.target.id;
+
+    this.setState({ [targetedProperty]: event.target.value }, () => {
+    });
+  }
+
+  validateUserInfo = (event) => {
+    event.preventDefault();
+    if (
+        this.state.name.length > 0 &&
+        this.state.date.length > 0 &&
+        this.state.time.length > 0 &&
+        this.state.number > 0
+      ) {
+
+        console.log('Form is ready!!!');
+        this.setState({ formReady: true });
+        this.props.addUserInputToStateOfApp({
+          name: this.state.name,
+          date: this.state.date,
+          time: this.state.time,
+          number: this.state.number,
+        })
+      }
   }
 
   render() {
     return (
       <div className="form-container">
         <form>
-          <input placeholder="Name"></input>
-          <input placeholder="Date (mm/dd)"></input>
-          <input placeholder="Time"></input>
-          <input placeholder="Number of Guests"></input>
-          <button>Make Reservation</button>
+          <input id="name" placeholder="Name" onChange={this.updateStateWithUserInput}></input>
+          <input id="date" placeholder="Date (mm/dd)" onChange={this.updateStateWithUserInput}></input>
+          <input id="time" placeholder="Time" onChange={this.updateStateWithUserInput}></input>
+          <input type="number" id="number" placeholder="Number of Guests" onChange={this.updateStateWithUserInput}></input>
+          <button type="submit" onClick={this.validateUserInfo}>Make Reservation</button>
         </form>
       </div>
     )
